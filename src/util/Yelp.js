9bd866c368d08559c395f50795b5bf599a79e604
Danijela <danijela@tcom.rs>
2020-02-04 16:17:35 +0100

    Yelp API added

---
+++ src/util/Yelp.js
@@ -0,0 +1,35 @@
+const apiKey = '8BqGVv--ZgJgRjWfPGKhLc-v7ogxroXy2buDJrK81VFqNB3jcfMirERI8snbh14SO9_DDG1CLKMt3gni1WpZ6eI-CZZW-cR1BDvFOoAERqKUH_eU7aJVSDM5q4A5XnYx';
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`
+    
+                headers: {
+                    Authorization: `Bearer ${apiKey}`,
+                },
+            }).then(response => {
+                return response.json();
+            }).then(jsonResponse => {
+                if (jsonResponse.businesses) {
+                    return jsonResponse.businesses.map(((business) => {
+                        return {
+                            id: business.id,
+                            imageSrc: business.image_url,
+                            name: business.name,
+                            address: business.location.address1,
+                            city: business.location.city,
+                            state: business.location.state,
+                            zipCode: business.location.zip_code,
+                            category: business.categories[0].title,
+                            rating: business.rating,
+                            reviewCount: business.review_count
+                        };
+                    }));
+                }
+            })
+    }
+
+};
+
+
+export default Yelp;
